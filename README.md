ac429e16122d20e017396887e9ef0e40325e91b5
Shrimadhav U K <shrimadhavuk@gmail.com>
2017-03-15 22:19:49 +0530

    add README

---
+++ README.md
@@ -0,0 +1,3 @@
+## To `sudo` Or Not To `sudo`
+
+Read more about this repo [here](https://blog.shrimadhavuk.me/posts/to-sudo-or-not-to-sudo/)!
--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='ForTheGreaterGood',    # This is the name of your PyPI-package.
     version='0.1',                          # Update the version number for new releases
-    scripts=['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
+    scripts=['helloworld.py']                  # The name of your scipt, and also the command you'll be using for calling it
 )
